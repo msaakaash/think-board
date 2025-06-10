8d335d5c158a1a5aa73e7b5ad756750b7d2ccfb0
msaakaash <msaakaash@hotmail.com>
2025-06-10 23:03:39 +0530

    Basic route setup

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/api/v1/notes",(req,res)=>{
+    res.send("Your notes..")
+})
+
 app.listen(5001,()=>{
     console.log("Server started running..");
     
