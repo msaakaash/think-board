48a050638eefd5a4386fa675bec03e0ef1f3dc83
msaakaash <msaakaash@hotmail.com>
2025-06-12 07:30:21 +0530

    Database connection initiated

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,19 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
-
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-
+const PORT = process.env.PORT || 5001;
+connectDB();
 app.use("/api/v1/notes",notesRoutes);
 
 app.get("/api/v1/notes",(req,res)=>{
     res.send("Your notes..")
 })
 
-app.listen(5001,()=>{
-    console.log("Server started running..");
+app.listen(PORT,()=>{
+    console.log(`Server started running on http://localhost:{PORT}`);
     
 })
