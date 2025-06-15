13077f94dbabc528e0f898b8d62ccb4a280afe1a
msaakaash <msaakaash@hotmail.com>
2025-06-15 13:38:05 +0530

    Added ratelimiting with upstash

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,19 +3,23 @@ import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import swaggerDocs from './docs/swagger.js';
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-connectDB();
+
 
 app.use(express.json())
+app.use(rateLimiter)
 
 app.use("/api/v1/notes",notesRoutes);
 
 swaggerDocs(app);
 
-app.listen(PORT,()=>{
-    console.log(`Server started running on http://localhost:${PORT}`);
-    
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+    console.log(`Server started running on http://localhost:${PORT}`);  
 })
+})
+
