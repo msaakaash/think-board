44c3494e7a9d0d9f8f02c0012026a5a9b648ea42
msaakaash <msaakaash@hotmail.com>
2025-06-16 19:28:24 +0530

    Added Axios

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,14 +4,20 @@ import { connectDB } from "./config/db.js";
 import swaggerDocs from './docs/swagger.js';
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
 
+app.use(cors({
+    origin: "http://localhost:5173",
+}))
 app.use(express.json())
 app.use(rateLimiter)
+
 
 app.use("/api/v1/notes",notesRoutes);
 
