b36aa073e6737a5bdb67e5cbd46c7d8398fceb31
msaakaash <msaakaash@hotmail.com>
2025-06-17 19:32:12 +0530

    Backend done

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,47 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import swaggerDocs from './docs/swagger.js';
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
-import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
 
-app.use(cors({
-    origin: "http://localhost:5173",
-}))
-app.use(express.json())
-app.use(rateLimiter)
+// our simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
+// });
 
+app.use("/api/notes", notesRoutes);
 
-app.use("/api/v1/notes",notesRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-swaggerDocs(app);
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
-connectDB().then(()=>{
-    app.listen(PORT,()=>{
-    console.log(`Server started running on http://localhost:${PORT}`);  
-})
-})
-
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
+});
